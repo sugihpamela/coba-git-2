63bdaff768a7f908f9f4c6fc58d8f28284e51e3b
Sugih Pamela <sugihpamela@gmail.com>
2024-05-29 01:44:13 +0700

    add index.php

---
+++ mahasiswa.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Coba GIT</title>
+</head>
+<body>
+    <h3>Data Mahasiswa</h3>
+    <ul>
+        <li>Nama : Sugih Pamela</li>
+        <li>Umur : 27 Tahun</li>
+    </ul>
+</body>
+</html>
